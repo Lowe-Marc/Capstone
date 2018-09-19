968aad2acb27d7e03ab4d187cfaa901427de721d
Lowe-Marc <Marcus.S.Lowe@gmail.com>
2018-09-18 22:08:14 -0500

    Bug fix for Chrome turning the screen black when clicking fullscreen

--- Capstone/js/Custom/ui-helpers.js
+++ Capstone/js/Custom/ui-helpers.js
@@ -1,12 +1,4 @@
 
-function configurationOverlayOn() {
-    document.getElementById("simulation-config-overlay").style.display = "block";
-}
-
-function configurationOverlayOff() {
-    document.getElementById("simulation-config-overlay").style.display = "none";
-}
-
 function setAStarConfig() {
     // $('#simulation-config-overlay').load('..\\Views\\Configurations\\AStarConfiguration.cshtml')
     var content = '<div> Start: </div>';
